3d2f300d516eaa8c14ac0eb7cc1fce4e3fea8e0e
MarianeBS <mariane.souza22@etec.sp.gov.br>
2024-03-24 01:06:29 -0300

    SOS

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const handlebars = require("express-handlebars").engine
+const bodyParser = require("body-parser")
+const post = require("./models/post")
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get("/", function(req, res){
+    res.render("primeira_pagina")
+})
+
+app.post("/cadastrar", function(req, res){
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function(){
+        res.send("Dados enviados com sucesso!")
+    }).catch(function(erro){
+        res.send("Falha ao cadastrar os dados" + erro)
+    })
+})
+
+app.listen(8081, function(){
+    console.log("Servidor ativo!")
+})
